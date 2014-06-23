f1759d1faaeab658fe6f67db030f78778a43f8d1
dlsso <supertonesfan@hotmail.com>
2014-06-23 12:29:02 -0600

    Did all string computations

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 var word = prompt("Please enter a word");
 
 alert("Your word: " + word + "\n"
-	+ "Other things:");
+	+ "It is " + word.length + " characters long." + "\n"
+	+ "The third character is " + word.charAt(2) + ".\n"
+	+ "Your word in lowercase is " + word.toLowerCase() + ".\n"
+	+ "Your word in uppercase is " + word.toUpperCase() + ".\n"
+	+ "Example: I would really like a " + word + " right now.\n"
+	+ "Subword: " + word.substring(1,4));
